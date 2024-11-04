9b570355b5deec0aecff802d8865c3526ba2dc7b
Shanna Wallace <shannaw04@yahoo.com>
2024-11-03 20:19:41 -0500

    chat box

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "7594524"
+    serviceWorkerVersion: "1987334418"
   }
 });
